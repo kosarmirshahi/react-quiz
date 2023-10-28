e7effb8661ea8f2dabc27a64e82981ac5c959a07
kosar mirshahi <kosarmirshahi@gmail.com>
2023-10-28 13:05:54 +0330

    start page

---
+++ src/components/StarterPage/StarterPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+function StarterPage() {
+  return (
+    <div className="w-3/4 mx-auto mt-32 text-center text-white">
+      <h1 className="text-5xl mb-10">Welcome to the React Quiz</h1>
+      <p className="text-2xl mb-14">Let's go. Good luck!!</p>
+      <button className="bg-[#EEACFF] w-36 h-12 font-bold rounded">
+        Start
+      </button>
+    </div>
+  );
+}
+
+export default StarterPage;
